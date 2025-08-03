d1f0e774430f911a301019b72c82ee6e7de3cc3c
mobasshir <mobasshirkhan9931@gmail.com>
2025-08-03 21:15:29 +0530

    completed the session Page

---
+++ src/lib/motion.ts
@@ -0,0 +1,123 @@
+export interface MotionItem {
+  types: string;
+  motion: string;
+  InfoSlide: string;
+}
+
+export const motion: MotionItem[] = [
+  {
+    types: "Feminism Gender Social Policy",
+    motion: "This House believes that the feminist movement in the developing world should prioritise the masculinisation of private spaces over the feminisation of public spaces in its advocacy",
+    InfoSlide: "The masculinisation of private spaces refers to efforts to involve men in private life tasks (housework, care system, etc.). The feminisation of public spaces refers to efforts to involve women in public life (taking positions of power, joining the work force, etc.).",
+  },
+  {
+    types: "Democracy International Relations Social Policy",
+    motion: "In multiethnic states, This house prefers the use of devolved governments as opposed to centralised governments",
+    InfoSlide: "Devolution is the transfer of power from a central government to subnational (e.g., state, regional, or local) authorities. The devolved territories have the power to make legislation relevant to the area thus granting them a higher level of autonomy; this includes areas such as healthcare, law enforcement, education, etc.",
+  },
+  {
+    types: "Feminism Gender Social Policy",
+    motion: "This House believes that the feminist movement in the developing world should prioritise the masculinisation of private spaces over the feminisation of public spaces in its advocacy",
+    InfoSlide: "The masculinisation of private spaces refers to efforts to involve men in private life tasks (housework, care system, etc.). The feminisation of public spaces refers to efforts to involve women in public life (taking positions of power, joining the work force, etc.).",
+  },
+  {
+    types: "Democracy International Relations Social Policy",
+    motion: "In multiethnic states, This house prefers the use of devolved governments as opposed to centralised governments",
+    InfoSlide: "Devolution is the transfer of power from a central government to subnational (e.g., state, regional, or local) authorities. The devolved territories have the power to make legislation relevant to the area thus granting them a higher level of autonomy; this includes areas such as healthcare, law enforcement, education, etc.",
+  },
+  {
+    types: "Criminal Justice Privacy Social Policy",
+    motion: "In areas with high crime rates, This House would significantly relax protections on privacy (e.g. phone tapping, increased surveillance, etc)",
+    InfoSlide: "",
+  },
+  {
+    types: "Economics International Relations Middle East",
+    motion: "This House opposes the continued expansion of BRICS",
+    InfoSlide: "BRICS recently expanded to include Egypt, Ethiopia, Iran, the United Arab Emirates and Indonesia. BRICS is an acronym for the five major emerging economies: Brazil, Russia, India, China and South Africa. The group was formed to promote peace, security and development in a multipolar world.",
+  },
+  {
+    types: "Education/Academia Social Policy",
+    motion: "This House believes that schools should grant students greater individual autonomy over their school curricula and other learning processes (e.g. self-directed learning and increased self-assessment).",
+    InfoSlide: "",
+  },
+  {
+    types: "International Relations Military Security",
+    motion: "This house believes that the increasing military securitization of Australasia has done more harm than good",
+    InfoSlide: "Military securitization is a process of elevating military concerns and considerations to a central focus in a country's security policies and strategies. This often involves prioritizing military threats, enhancing defense capabilities, and increasing military investments to address perceived security challenges.",
+  },
+  {
+    types: "Economics International Relations Military",
+    motion: "This house opposes a world where the Military-Industrial Complex exists.",
+    InfoSlide: "The Military-Industrial Complex (MIC) refers to the intricate and often symbiotic relationship between a nation's military establishment, its government, and the private defense industries that supply weapons and military technology. This concept, famously coined by U.S. President Dwight D. Eisenhower in his 1961 farewell address, highlights the potential for this powerful alliance to exert undue influence on public policy, defense spending, and international relations. It suggests a vested interest in maintaining high levels of military expenditure and, at times, in perpetuating conflict, as it benefits all parties involved.",
+  },
+  {
+    types: "International Relations Military Politics",
+    motion: "This House predicts that the US will be involved in more wars than it is today by the end of Donald Trump's presidential term",
+    InfoSlide: ""
+  },
+  {
+    types: "Culture Military Philosophy",
+    motion: "This House would violate the Bushido Code.",
+    InfoSlide: " The samurai were warriors of the Japanese Empire, known for following a strict code of honor, Bushido, which demands from the samurai love for their nation, even at the cost of their own life in combat. Among other principles, Bushido values loyalty, courage, and honor. To this end, war should be just and straightforward, with strategies such as ambushes, betrayals, or traps outside open and honest confrontation being morally inadmissible. A dishonored samurai could suffer severe punishments, such as isolation, expulsion from service, and loss of citizenship. Infoslide 2: You are a samurai of the Japanese Empire and have been trained since childhood to fight following Bushido. However, your native island is about to be invaded by Mongol troops, known for their brutality, numerical superiority, and more advanced weaponry. This is not the first battle in which you face a militarily superior enemy; still, you know this will be a difficult battle, and your nation is gripped by apprehension and despair."
+  },
+  {
+    types: "International Relations Military",
+    motion: " This house believes that countries with substantial defense capabilities should actively intercept foreign military communications of post conflict states",
+    InfoSlide: ""
+  },
+  {
+    types: "Asia International Relations Military",
+    motion: "This house, as ASEAN, would recognize the Tatmadaw as the legitimate government of Myanmar",
+    InfoSlide: "The Tatmadaw, also known as the Sit-Tat, is the armed forces of Myanmar. Since 2021, it has been the de-facto ruling government in the country."
+  },
+  {
+    types: "Economics Military Philosophy",
+    motion: "This house, as Chonky, would take the job",
+    InfoSlide: "In 1942, Dr. Chonky is an unemployed theoretical physicist and is offered a job developing a new category of atomic weapons (seemingly not prohibited by any treaty). Chonky has always been strongly opposed to weapons of mass destruction, but jobs are scarce and Chonky has a family to provide for. Chonky's spouse makes a very modest salary as a preschool teacher, and has no qualms about Chonky working on these new weapons. If Chonky does not take the job, they are told that Dr. Tairanosaur, someone without any reservations about the weapons research, will take the job and will likely move the development ahead more quickly."
+  },
+  {
+    motion: "The council is of the opinion that exclusive art spaces bring more harm than good.",
+    InfoSlide: "Exclusive art spaces refer to exhibitions or installations that limit access to certain groups, usually based on identity. The Ladies' Lounge at the Museum of Old and New Art (MONA) in Tasmania, Australia, is an example of an exclusive art space that recently prohibited cisgender men from entering the exhibit. The museum subsequently faced a discrimination lawsuit, which led to the closure of the lounge.",
+    types: "Art Culture LGBTQ+"
+  },
+  {
+    motion: "This house believes that the queer movement in Jamaica should oppose rather than support the police",
+    InfoSlide: "Jamaica's anti-sodomy and public morality laws have long sanctioned violence and discrimination against queer people. In recent years, the queer movement in Jamaica and its advocacy groups (e.g., J-FLAG, TransWave) has grown more visible and influential. Although formal arrests are now rare and police sometimes provide limited protection for events like Pride, officers regularly intimidate and harass queer people.",
+    types: "LGBTQ+ Minority Communities Police"
+  },
+  {
+    motion: "This House opposes the focus on coming out in LGBTQ+ media and popular culture.",
+    InfoSlide: "",
+    types: "Culture LGBTQ+ Media"
+  },
+  {
+    motion: "As a reputable children's book author, this House would choose to queer code main characters in the story",
+    InfoSlide: "Queer coding is the act of using subtext to code a character in media as queer. The process of queer coding entails applying characteristics associated with queerness to a character, without ever confirming it.",
+    types: "Children LGBTQ+ Social Policy"
+  },
+  {
+    motion: "This house believes that the feminist movement should actively oppose the participation of gender and sexual minorities in hip-hop/rap",
+    InfoSlide: "",
+    types: "Culture Feminism LGBTQ+"
+  },
+  {
+    motion: "This house supports Western pop artists with significant queer fanbase actively rejecting to perform in countries with queer-oppressive social policies (e.g. Lizzo in Texas, Beyonce in UAE)",
+    InfoSlide: "",
+    types: "Culture International Relations LGBTQ+"
+  },
+  {
+    motion: "That we support the research into allowing parents to genetically ensure that they will have an LGBT baby.",
+    InfoSlide: "",
+    types: "Social Policy LGBTQ+ Children Science/Technology"
+  },
+  {
+    motion: "This house regrets mainstream media's popularization of Western drag culture",
+    InfoSlide: "",
+    types: "Culture LGBTQ+ Media"
+  },
+  {
+    motion: "This house supports Western pop artists with significant queer fanbase actively rejecting to perform in countries with queer-oppressive social policies (e.g. Lizzo in Texas, Beyonce in UAE)",
+    InfoSlide: "",
+    types: "Culture International Relations LGBTQ+"
+  }
+];
